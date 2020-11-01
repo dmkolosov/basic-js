3f3f453298fea580274c6a5a956263ed634fa3e5
dmkolosov <maker.bug.00@inbox.ru>
2020-11-01 14:27:59 +0300

    feat: implement 'Dream team' task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if(!Array.isArray(members)) return false
+  let result =[]
+  for (let index = 0; index < members.length; index++) {
+
+    if(typeof members[index] === 'string' ){
+
+      let a=members[index].trim()
+      if(a!=""){
+        result.push(a.trim()[0].toLowerCase())
+      }
+  
+  
+      
+    }
+  }
+  
+  return result.sort(sor).join("").toUpperCase()
 };
+
+
+
+
+  function sor(a,b){
+    if(a< b) { return -1; }
+    if(a> b) { return 1; }
+    return 0;
+  }
